27d7b1024871b7ba96f0700e168970234857e170
Daniel Holland <dholland@athenahealth.com>
2022-09-26 14:57:25 -0400

    rename package

--- tests/test_mynamespace.py
+++ tests/test_mynamespace.py
@@ -2,18 +2,18 @@
 
 
 def test_thing():
-    """Always passes."""
-    from testdots.cooltest import constants
+    """Sanity test that always passes."""
+    from mynamespace.mypackage import constants
     assert constants.THING == 3
 
 
 def test_version():
     """Fails on poetry 1.2.1, passes on 1.1.13."""
     import pkg_resources
-    assert pkg_resources.get_distribution('testdots.cooltest').version == '0.1.0'
+    assert pkg_resources.get_distribution('mynamespace.mypackage').version == '0.1.0'
 
 
 def test_version_dash_replace():
     """Passes on both versions of poetry."""
     import pkg_resources
-    assert pkg_resources.get_distribution('testdots-cooltest').version == '0.1.0'
+    assert pkg_resources.get_distribution('mynamespace-mypackage').version == '0.1.0'
